56463c61748573ae53cd81b439b5566f9a607ec5
Taranjeet Singh <taranjeets120@gmail.com>
2026-02-18 21:04:19 +0530

    signup auth api

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import { signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
-router.get('/signup', (req, res) => {
-  res.send('Hello World!')
-})
+router.post('/signup', signup)
 
-export default router
+export default router 
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
+import { connectDB } from './lib/db.js'
 dotenv.config()
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+app.use(express.json()) // req.body
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
@@ -15,4 +17,5 @@ app.use("/api/messages",messageRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
+  connectDB()
 })
