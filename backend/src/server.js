93b683ccab870d02418c9c3603bc749d6b9a8adc
Taranjeet Singh <taranjeets120@gmail.com>
2026-03-09 20:41:51 +0530

    fix production

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,19 @@ import { connectDB } from './lib/db.js'
 import { ENV } from './lib/env.js'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-
 const app = express()
 
 const PORT = ENV.PORT || 3000
+app.use(express.json()) // req.body
+app.use(cookieParser()) // req.cookies
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}))
+// app.use("/", (req, res) => {
+//     res.send("Welcome to Chatify API")
+// })
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
 
-// increase payload size
-app.use(express.json({ limit: "10mb" }))
-app.use(express.urlencoded({ limit: "10mb", extended: true }))
 
-app.use(cookieParser())
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
-
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
